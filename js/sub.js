2630cbfa39fc98f9a6ebab9e5962bb9e846afd90
Utilisatuer <utilisatuer@Utilisateurui-MacBookPro.local>
2025-02-23 16:12:46 +0900

    learning tracks page edit

--- js/sub.js
+++ js/sub.js
@@ -1,36 +1,27 @@
 
-/* 팀별 일정 상단 버튼 */
-
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.scd-btn-wrap .btn');
-    const scdBoxes = document.querySelectorAll('.scd-box');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            // 모든 박스를 숨김
-            scdBoxes.forEach(box => {
-                box.style.display = 'none';
-            });
-
-            // 모든 버튼의 opacity를 0.3으로 설정
-            buttons.forEach(btn => {
-                btn.style.opacity = '.3';
-            });
-
-            // 클릭한 버튼의 opacity를 1로 설정
-            button.style.opacity = '1';
-
-            // 클릭한 버튼의 클래스 이름에 따라 해당 박스를 표시
-            const targetClass = button.classList[1]; // btn-a, btn-b 등
-            const targetBox = document.querySelector(`.${targetClass.replace('btn-', 'scd-box-')}`); // scd-box-a, scd-box-b 등
-
-            // 해당 박스를 표시
-            if (targetBox) {
-                targetBox.style.display = 'grid';
+/* 팀별 일정 */
+document.addEventListener('DOMContentLoaded', () => {
+    const detailsElements = document.querySelectorAll('details');
+  
+    detailsElements.forEach(details => {
+      details.addEventListener('click', (event) => {
+        if (details.open) {
+          event.preventDefault();
+          details.classList.add('closing');
+          setTimeout(() => {
+            details.open = false;
+            details.classList.remove('closing');
+          }, 300);
+        } else {
+          detailsElements.forEach(otherDetails => {
+            if (otherDetails !== details && otherDetails.open) {
+              otherDetails.open = false;
             }
-        });
+          });
+        }
+      });
     });
-});
+  });
 
 /* 키노트 스피커 */
 document.addEventListener("DOMContentLoaded", () => {
